8db456bf5d57cdc5523d9740ad44400569f7fd0a
Kevin McDermott <bigkevmcd@gmail.com>
2020-06-29 08:59:11 +0100

    Fix the test logger.

--- test/logger.go
+++ test/logger.go
@@ -1,12 +1,14 @@
 package test
 
 import (
+	"testing"
+
 	"go.uber.org/zap"
 	"go.uber.org/zap/zaptest"
 )
 
 // MakeTestLogger creates a logger than uses zaptest.
-func MakeTestLogger() logger.Logger {
+func MakeTestLogger(t *testing.T) *zap.SugaredLogger {
 	logger := zaptest.NewLogger(t, zaptest.Level(zap.WarnLevel))
 	return logger.Sugar()
 }
