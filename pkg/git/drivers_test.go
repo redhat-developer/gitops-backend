c4130635e3e07ac9e853d4cb235fc9ed8ddda365
Kevin McDermott <bigkevmcd@gmail.com>
2020-07-27 10:51:38 +0100

    Add support for customising the endpoint identification.

--- pkg/git/drivers_test.go
+++ pkg/git/drivers_test.go
@@ -10,18 +10,29 @@ var _ DriverIdentifier = (*URLDriverIdentifier)(nil)
 
 func TestIdentify(t *testing.T) {
 	urlTests := []struct {
-		gitURL  string
-		want    string
-		wantErr string
+		gitURL   string
+		want     string
+		readFile string
+		wantErr  string
 	}{
-		{"https://github.com/myorg/myrepo.git", "github", ""},
-		{"https://gitlab.com/myorg/myrepo/myother.git", "gitlab", ""},
-		{"https://scm.example.com/myorg/myother.git", "", "unable to identify driver"},
+		{"https://github.com/myorg/myrepo.git", "github", "", ""},
+		{"https://gitlab.com/myorg/myrepo/myother.git", "gitlab", "", ""},
+		{"https://gl.example.com/myorg/myrepo/myother.git", "gitlab", "testdata/drivers.yaml", ""},
+		{"https://scm.example.com/myorg/myother.git", "", "", "unable to identify driver"},
+		{"https://u.example.com/myorg/myrepo/myother.git", "", "doesnotexist.yaml", "failed to open"},
 	}
 
-	identifier := NewDriverIdentifier()
 	for _, tt := range urlTests {
 		t.Run(tt.gitURL, func(rt *testing.T) {
+			identifier := NewDriverIdentifier()
+			if tt.readFile != "" {
+				err := identifier.AddDriversFromFile(tt.readFile)
+				if !test.MatchError(rt, tt.wantErr, err) {
+					rt.Errorf("got an error reading the drivers file %s: %s", tt.readFile, err)
+				}
+				return
+			}
+
 			driver, err := identifier.Identify(tt.gitURL)
 			if !test.MatchError(rt, tt.wantErr, err) {
 				rt.Errorf("error failed to match, got %#v, want %s", err, tt.wantErr)
