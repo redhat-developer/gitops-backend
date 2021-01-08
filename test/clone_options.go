8049ab310e7ea22b67c45a9520b67fe2c5837ac4
Kevin McDermott <bigkevmcd@gmail.com>
2021-01-08 07:31:04 +0000

    Create go.yml (#5)
    Switch to using GitHub actions and drop the Travis from the CI tests.

--- test/clone_options.go
+++ test/clone_options.go
@@ -7,14 +7,20 @@ import (
 	"github.com/go-git/go-git/v5/plumbing"
 )
 
-// MakeCloneOptions determines if we are running in Travis,
+// MakeCloneOptions determines if we are running in GitHub,
 // and ensures that it's using the correct branch.
+//
+// This is because the codebase that the tests run in is not a clone, so we have
+// to clone the upstream.
+//
+// For local development, you're working on the branch, and we can use that
+// branch correctly.
 func MakeCloneOptions() *git.CloneOptions {
 	o := &git.CloneOptions{
 		URL:   "../..",
 		Depth: 1,
 	}
-	if b := os.Getenv("TRAVIS_PULL_REQUEST_BRANCH"); b != "" {
+	if b := os.Getenv("GITHUB_BASE_REF"); b != "" {
 		o.ReferenceName = plumbing.NewBranchReferenceName(b)
 		o.URL = "https://github.com/redhat-developer/gitops-backend.git"
 	}
